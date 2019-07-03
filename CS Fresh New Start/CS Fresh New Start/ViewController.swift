7ba6b49dc7c9049351fecd0f48f5612b638623a2
josediaz99 <52419380+josediaz99@users.noreply.github.com>
2019-07-03 13:44:26 -0500

    Add files via upload

---
+++ CS Fresh New Start/CS Fresh New Start/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS Fresh New Start
+//
+//  Created by SER-YCL6 on 7/3/19.
+//  Copyright © 2019 affirmation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
